0c2ae8fa3b7f756da56fbc6ebf661ce7d95a154a
leZen <112973781+leZenfr@users.noreply.github.com>
2025-06-21 10:19:24 +0200

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+source /opt/activevision/bin/activate
+
+nohup python3 /srv/event-agent.py > /var/log/event.log 2>&1 &
+nohup python3 /srv/object-agent.py > /var/log/object.log 2>&1 &
+
+echo "Les agents sont démarrés en arrière plan"
