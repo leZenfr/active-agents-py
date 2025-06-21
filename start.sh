0cdfa518179c25d5579d8fd29339db9f8d4dbf62
leZen <112973781+leZenfr@users.noreply.github.com>
2025-06-21 13:33:19 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,10 +4,10 @@ source /opt/activevision/bin/activate
 
 echo "Démarrage des agents..."
 
-sudo nohup /opt/activevision/bin/python3 /srv/event-agent.py > /var/log/event.log 2>&1 &
+nohup /opt/activevision/bin/python3 /srv/event-agent.py > /var/log/event.log 2>&1 &
 echo $! > /var/run/event-agent.pid
 
-sudo nohup /opt/activevision/bin/python3 /srv/object-agent.py > /var/log/object.log 2>&1 &
+nohup /opt/activevision/bin/python3 /srv/object-agent.py > /var/log/object.log 2>&1 &
 echo $! > /var/run/object-agent.pid
 
 echo "✅ Les agents sont démarrés en arrière-plan."
